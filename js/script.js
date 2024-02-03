d75a3f9862e3e495eb129e6f29e950036ee0ed06
ahmed666916 <ahmed666916@gmail.com>
2024-02-03 21:42:43 +0500

    created two furthur containers inside button-container

--- js/script.js
+++ js/script.js
@@ -3,10 +3,17 @@ const buttonTexts = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", ".", "="]
 
 const btn_container = document.getElementById("button-container");
 
+const num_container = document.getElementById("number-container");
+const operator_container = document.getElementById("operator-container");
+
 buttonTexts.forEach((text, index) => {
     const button = document.createElement("button");
     button.id = `button${index + 1}`;
     button.classList.add("button");
     button.textContent = text;
-    btn_container.appendChild(button);
+    num_container.appendChild(button);
 })
+
+
+btn_container.appendChild(num_container);
+btn_container.appendChild(operator_container);
