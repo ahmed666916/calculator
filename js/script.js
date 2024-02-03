e13a098d826bcbe0074a3e97d5e607880440b9ab
ahmed666916 <ahmed666916@gmail.com>
2024-02-03 21:11:09 +0500

    created buttons dynamically and set flex-direction for container to be column

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+const buttonTexts = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", ".", "="];
+
+const btn_container = document.getElementById("button-container");
+
+buttonTexts.forEach((text, index) => {
+    const button = document.createElement("button");
+    button.id = `button${index + 1}`;
+    button.textContent = text;
+    btn_container.appendChild(button);
+})
